ec248c51bedc7b069f75a479f7bc161d8b9a99de
Martin Régner <46482115+martinregner@users.noreply.github.com>
2021-10-21 15:43:59 +0200

    ajout statuts simplifiés

--- src/direflow-components/vertvolt-component/utils/setTooltipStatutOffre.js
+++ src/direflow-components/vertvolt-component/utils/setTooltipStatutOffre.js
@@ -4,6 +4,8 @@ export default function (statut) {
     'Labélisation après audit': `Le fournisseur a démontré son respect des critères du label VertVolt.`,
     'Labélisation suspendue contrôle en cours': `Les éléments de preuve du fournisseur vis-à-vis du respect des critères du label VertVolt ont été jugés insuffisants.`,
     Déclaratif: `Le fournisseur s'est engagé à respecter les critères du label VertVolt.`,
+    'Audit réalisé': `Le fournisseur a démontré son respect des critères du label VertVolt.`,
+    'Suspension pour contrôle supplémentaire': `Les éléments de preuve du fournisseur vis-à-vis du respect des critères du label VertVolt ont été jugés insuffisants.`,
   }
   return labels[statut]
 }
